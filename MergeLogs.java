806897ac7278038631dcda50a2cda4e56e08abf4
lavamunky <peter@lavamunky.com>
2013-07-09 17:17:32 +0100

    Much improved the parsing of command line, via apache commons cli

--- MergeLogs.java
+++ MergeLogs.java
@@ -1,9 +1,96 @@
+import org.apache.commons.cli.*;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Properties;
+
 public class MergeLogs
 {
-    static int option;
-    static String outFile;
+    
+    
+
+
     public static void main(String[] args)
     {
+        //Option separateLog = new Option("f", "output", true, "Prints the merged instance to a separate log file");
+        //creating options
+        
+        Properties properties = new Properties();
+        properties.setProperty("OUT", "output");
+        properties.setProperty("COMBINE", "combine");
+        properties.setProperty("HELP", "help");
+        
+        final Option separateLog = OptionBuilder.withArgName("filename")
+                             .hasArg()
+                             .isRequired(false)
+                             .withDescription("Prints the merged instance to a separate log file, filename")
+                             .withLongOpt("output")
+                             .create("o");
+        final Option combine = OptionBuilder.isRequired(false)
+                        .withDescription("Prints the merged log into the first log given")
+                        .withLongOpt("combine")
+                        .create("s");
+        final Option help = OptionBuilder.isRequired(false)
+                            .withDescription("Prints this message")
+                            .withLongOpt("help")
+                            .create("h");
+
+        //above arguments are mutually exclusive, so put in OptionGroup
+        OptionGroup optgrp = new OptionGroup();
+        optgrp.addOption(separateLog);
+        optgrp.addOption(combine);
+        
+        Options options = new Options();
+        options.addOptionGroup(optgrp);
+        options.addOption(help);
+
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = null;
+        
+        //System.out.println("debug 0");
+
+        try {
+            cmd = parser.parse(options, args);
+
+        } catch (ParseException pe){
+            System.out.println("Parsing command line failed. Reason: " + pe.getMessage());
+            summary(options);
+        } 
+        //NOTE TO SELF: NEED TO CHECK FOR FILENAME ON separateLog
+
+        //System.out.println("debug 1");
+
+        //Debugging purposes
+        Option[] commands = cmd.getOptions();
+        List files = cmd.getArgList();
+
+        //System.out.println("debug 2");
+
+        if (files.size()!=2){
+            System.out.println("Wrong number of arguments");
+            summary(options);
+        }
+
+        //System.out.println("debug 3");
+      
+        for (Option opt: commands)
+        {
+            System.out.println("getArgName: " + opt.getArgName());
+            System.out.println("getOpt: " + opt.getOpt());
+            System.out.println("getValue: " + opt.getValue());
+            if (opt.equals(separateLog)){
+                System.out.println("equals separateLog");
+            } else if (opt.equals(combine)){
+                System.out.println("equals combine");
+            }
+        }
+        if (commands.length == 0)
+            System.out.println("stdout");
+
+        
+
+        //summary(options);
+
+        /*
         String[] files = new String[3]; // [outputFile, firstMergeFile, secondMergeFile]
         //parse command line
         files = parseCommandLine(args);
@@ -11,80 +98,19 @@ public class MergeLogs
 
         for (String str : files)
             System.out.println(str);
+        */
 
         //parse start of lines
 
         //merge by date & time
 
     }
-
-    static String[] parseCommandLine(String[] cli)
+    
+    static void summary(Options opts)
     {
-        if (cli.length<2 || cli.length>4)
-            summary();
-        
-        int index = parseOptions(cli);
-        //validation of number of arguments given
-        if (option==0) //no switches given, should be 2 args
-        {
-            if (cli.length!=2) {summary();};
-        }
-        else if (option==1) //-f logfile.log set, should be 4 args
-        {
-            if (cli.length!=4) {summary();};
-        }
-        else if (option==2) //-s set, should be 3 args
-        {
-            if (cli.length!=3) {summary();};
-        }
-        if (option==0)
-            outFile=cli[index];
-        else if (option==2)
-            outFile=null;
-        String[] files = {outFile, cli[index], cli[index+1]};
-        return files;
-    }
-
-    static int parseOptions(String[] cli)
-    {
-        option=0;
-        int index=0; //index of argument after any options
-        for (int i=0; i<cli.length; i++)
-        {
-            if (cli[i].charAt(0) == '-') //a switch
-            {
-                if (option!=0) //already changed, therefore multiple switches. Not currently allowed
-                    summary();
-                if (cli[i].length()>2) //not particularly allowing for expandibility. But there are better ways to parse command line args
-                {
-                    System.out.println("Only one option allowed at a time");
-                    summary();
-                }
-                switch (cli[i].charAt(1)){ //check if one of the known options. Very few, so quicker
-                    case 'f':   if (cli.length>(i+1) && cli[i+1] != null)
-                                {
-                                    outFile=cli[i+1];
-                                    option=1;
-                                    index=i+2; //takes additional argument
-                                }else
-                                    summary();
-                                break;
-                    case 's':   option=2;
-                                index=i+1; //takes no additional arguments
-                                break;
-                    default:    summary();
-                                break;
-                }
-            }
-        }
-        return index;
-
-    }
-    static void summary()
-    {
-        System.out.println("\tThis software is intended for merging logs, specifically those for Fortify 360 Server/HP Fortify Software Security Center");
-        System.out.println("\tIt looks at the date of each line and then combines the logs, either to stdout, the first log, or another log");
-        System.out.println("\tMergeLogs [OPTIONS] log1.log log2.log\n\t-f logfile.log\tPrints the merged instance to a separate log file, in this case logfile.log\n\t-s\t\tPrints the merged log into the first log given\n\nIf no switches are given, the merged log is printed to stdout\n");
+        //generate help statement
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("MergeLogs logfile_1 logfile_2", opts, true);
         System.exit(1);
     }
 }
